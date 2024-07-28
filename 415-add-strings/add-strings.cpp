31cf1ce33842833d49dad8e7fa8ef0c4a5d89f70
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2024-07-29 03:43:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (41.83%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -13,15 +13,16 @@ public:
         int sum = n1+n2+carry;
         int digit = sum%10;
         carry = sum/10;
+        add(num1, p1-1, num2, p2-1, carry, ans);
         ans.push_back(digit+'0');
 
-        add(num1, p1-1, num2, p2-1, carry, ans);
+        
     }
     string addStrings(string num1, string num2) {
         string ans = "";
         add(num1,num1.length()-1,num2, num2.length()-1,0,ans);
 
-        reverse(ans.begin(),ans.end());
+        //reverse(ans.begin(),ans.end());
 
         return ans;
 
