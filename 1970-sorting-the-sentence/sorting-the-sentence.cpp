d995bc94f6a06b85e1b06767f2f12709de51021e
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2025-04-10 01:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (87.62%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.cpp
+++ 1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-    static bool comp(string s1, string s2){
-        int n1=s1.length();
-        int n2=s2.length();
-
-        if(s1[n1-1]<s2[n2-1]){
-            return true;
-        }
-
-        return false;
-    }
     string sortSentence(string s) {
         int index=0;
         int start=0;
         string answer="";
-        vector<string> v;
+        vector<string> v(10,"");
         while(index<s.length()){
             start=index;
             while(index<s.length() && s[index]!=' '){
                 index++;
             }
             string str=s.substr(start,index-start);
-            v.push_back(str);
+            v[s[index-1]-'0']=str;
 
             index++;
         }
-        sort(v.begin(),v.end(),comp);
 
         for(string str:v){
-            answer+=(str.substr(0,str.length()-1));
-            answer.push_back(' ');
+            if(str!=""){answer+=(str.substr(0,str.length()-1));
+            answer.push_back(' ');}
         }
         answer.pop_back();
 
