9cc709cc6230483b986315d0d00d67370932e10a
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2025-05-04 01:27:51 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (97.81%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,21 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int height(TreeNode* root){
-        if(root==NULL){
+    int height(TreeNode* root, bool& answer){
+        if(root==NULL || !answer){
             return 0;
         }
 
-        int leftHeight=height(root->left);
-        int rightHeight=height(root->right);
+        int leftHeight=height(root->left, answer);
+        int rightHeight=height(root->right, answer);
         int maxHeight=max(leftHeight,rightHeight)+1;
 
-        
+        if(abs(leftHeight-rightHeight)>1){
+            answer=false;
+        }
 
         return maxHeight;
 
@@ -28,19 +31,10 @@ public:
 
     }
     bool isBalanced(TreeNode* root) {
-        if(root==NULL){
-            return true;
-        }
+        bool answer=true;
 
-        bool leftAns=isBalanced(root->left);
-        bool rightAns=isBalanced(root->right);
+        height(root, answer);
 
-        int heightDiff=abs(height(root->left)-height(root->right));
-
-        if(leftAns && rightAns && heightDiff<=1){
-            return true;
-        }
-
-        return false;
+        return answer;
     }
 };
