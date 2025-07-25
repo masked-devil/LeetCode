55b54c10081d22ed0a5884a5c78467d9cb82d064
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2025-07-26 02:21:49 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (54.73%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,88 +1,42 @@
 class Solution {
 public:
-    int getPivotIndex(vector<int>& nums){
-        int n=nums.size()-1;
-        int pivotIndex=n;
-        int start=nums[0];
 
+    bool search(vector<int>& nums, int target) {
         int low=0;
-        int high=n;
+        int high=nums.size()-1;
 
-        while(low<=high){
-            int mid=low+(high-low)/2;
-
-            if(mid<n && nums[mid]>nums[mid+1]){
-                pivotIndex=mid;
-                break;
-            }
-            else if(nums[mid]<start){
-                high=mid-1;
-            }
-            else if(nums[mid]>start){
-                low=mid+1;
-            }
-            else if(low<=n && high>=0){
-                if(nums[high]==nums[low]){
-                    if(low<n && nums[low]>nums[low+1]){
-                        pivotIndex=low;
-                        break;
-                    }
-                    else if(high<n && nums[high]>nums[high+1]){
-                        pivotIndex=high;
-                        break;
-                    }
-                    high--;
-                    low++;
-                }
-                else if(nums[low]==start){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-            }
-        }
-        return pivotIndex;
-    }
-    bool binarySearch(vector<int>& nums, int target, int low, int high){
         while(low<=high){
             int mid=low+(high-low)/2;
 
             if(nums[mid]==target){
                 return true;
             }
-            else if(nums[mid]<target){
-                low=mid+1;
+            //left is sorted
+            else if(nums[mid]>nums[low]){
+                //target in sorted
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            //right is sorted
+            else if(nums[mid]<nums[low]){
+                //target in sorted
+                if(target>nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
             }
             else{
-                high=mid-1;
+                //num[mid]==nums[low], can't decide sorted part, skip duplicate
+                low++;
             }
-            
         }
         return false;
-    }
-    bool search(vector<int>& nums, int target) {
-        bool  ans;
-
-        int pivotIndex=getPivotIndex(nums);
-
-        int low, high;
-
-        if(target<nums[0]){
-            low=pivotIndex+1;
-            high=nums.size()-1;
-        }
-        else if(target>nums[0]){
-            low=0;
-            high=pivotIndex;
-        }
-        else{
-            return true;
-        }
-
-        ans=binarySearch(nums,target,low, high);
-
-        return ans;
         
     }
 };
