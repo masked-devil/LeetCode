593e6be9956f60d639acf3cbdbf477a345f7a3dc
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2025-08-13 00:12:30 +0530

    Time: 14 ms (24.19%) | Memory: 22.9 MB (73.39%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    bool predicate(vector<int>& piles, long long k,int h){
+    bool predicate(vector<int>& piles, int mid, int h){
         long long hours=0;
 
-        for(auto x:piles){
-            if(x%k==0){
-                hours+=(x/k);
-            }
-            else{
-                hours+=(x/k+1);
-            }
-            if(hours>h) return false;
+        for(int x:piles){
+            hours+=(ceil(1.0*x/mid));
         }
-        if(hours>h) return false;
 
-        return true;
+        return hours>h?false:true;
+
     }
     int minEatingSpeed(vector<int>& piles, int h) {
+        int high=*max_element(piles.begin(),piles.end());
         int low=1;
-        int high=*(max_element(piles.begin(),piles.end()));
         int ans=high;
+
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(predicate(piles,mid,h)){
+
+            if(predicate(piles, mid, h)){
                 ans=mid;
                 high=mid-1;
             }
@@ -30,7 +26,7 @@ public:
                 low=mid+1;
             }
         }
-        return ans;
 
+        return ans;
     }
 };
