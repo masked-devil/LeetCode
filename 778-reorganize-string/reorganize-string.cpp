b922a8a5b5c7d0a4afc19e3194baa4d7185046bd
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2025-12-06 15:44:09 +0530

    Time: 3 ms (15.67%) | Memory: 8.4 MB (50.74%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,62 @@
+class Info {
+public:
+    char c;
+    int freq;
+
+    Info(char c, int freq) {
+        this->c = c;
+        this->freq = freq;
+    }
+};
+
+class Compare {
+public:
+    bool operator()(Info a, Info b) { return a.freq < b.freq; }
+};
+
+class Solution {
+public:
+    string reorganizeString(string s) {
+        priority_queue<Info, vector<Info>, Compare> maxHeap;
+        unordered_map<char, int> mp;
+        string ans = "";
+        for (auto x : s) {
+            mp[x]++;
+            if (mp[x] > (s.length() + 1) / 2) {
+                return "";
+            }
+        }
+
+        for (auto x : mp) {
+            Info temp(x.first, x.second);
+            maxHeap.push(temp);
+        }
+
+        while (maxHeap.size()>1) {
+            Info top1 = maxHeap.top();
+            maxHeap.pop();
+            Info top2 = maxHeap.top();
+            maxHeap.pop();
+
+            ans.push_back(top1.c);
+            ans.push_back(top2.c);
+
+            top1.freq--;
+            top2.freq--;
+
+            if(top1.freq > 0){
+                maxHeap.push(top1);
+            }
+            if(top2.freq > 0){
+                maxHeap.push(top2);
+            }
+        }
+
+        if (ans.length() < s.length()) {
+            Info maxi = maxHeap.top();
+            ans.push_back(maxi.c);
+        }
+
+        return ans;
+    }
+};
