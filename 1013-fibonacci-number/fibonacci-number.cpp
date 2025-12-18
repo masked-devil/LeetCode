ff1338f32df343e48ca565f64d7ac914b80ea2fc
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2025-12-18 22:46:34 +0530

    Time: 3 ms (62.47%) | Memory: 8 MB (19.69%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    int solveUsingMem(int n, vector<int>& dp){
+        if(n<=1){
+            return n;
+        }
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        dp[n] = solveUsingMem(n-1,dp) + solveUsingMem(n-2,dp);
+
+        return dp[n];
+
+    }
     int solveUsingRecursion(int n){
         if(n<=1){
             return n;
@@ -10,6 +24,8 @@ public:
         return ans;
     }
     int fib(int n) {
-        return solveUsingRecursion(n);
+        vector<int> dp(n+1,-1);
+        // return solveUsingRecursion(n);
+        return solveUsingMem(n,dp);
     }
 };
