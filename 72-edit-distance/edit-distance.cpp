d8520867570130bda1785934417de8e7866eb4c0
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2026-01-01 03:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (21.31%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int solveUsingRec(string& word1, string& word2, int a, int b){
+        if(a==word1.length()){
+            return word2.length()-b;
+        }
+        if(b==word2.length()){
+            return word1.length()-a;
+        }
+
+        if(word1[a] == word2[b]){
+            return solveUsingRec(word1,word2,a+1,b+1);
+        }
+        else{
+            int repl = 1 + solveUsingRec(word1,word2,a+1,b+1);
+            int del = 1+solveUsingRec(word1,word2,a+1,b);
+            int ins = 1+solveUsingRec(word1,word2,a,b+1);
+
+            return min({repl,del,ins});
+        }
+    }
+    int solveUsingMem(string& word1, string& word2, int a, int b, vector<vector<int>>& dp){
+        if(a==word1.length()){
+            return word2.length()-b;
+        }
+        if(b==word2.length()){
+            return word1.length()-a;
+        }
+        if(dp[a][b]!=-1){
+            return dp[a][b];
+        }
+        if(word1[a] == word2[b]){
+            dp[a][b]= solveUsingMem(word1,word2,a+1,b+1,dp);
+        }
+        else{
+            int repl = 1 + solveUsingMem(word1,word2,a+1,b+1,dp);
+            int del = 1+solveUsingMem(word1,word2,a+1,b,dp);
+            int ins = 1+solveUsingMem(word1,word2,a,b+1,dp);
+
+            dp[a][b]= min({repl,del,ins});
+        }
+
+        return dp[a][b];
+    }
+    int minDistance(string word1, string word2) {
+        // return solveUsingRec(word1,word2,0,0);
+        vector<vector<int>> dp(word1.length()+1, vector<int>(word2.length()+1,-1));
+        return solveUsingMem(word1,word2,0,0,dp);
+    }
+};
