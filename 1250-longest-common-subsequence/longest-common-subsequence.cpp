e10c6aad50c928727d0772c1bc4b2bc8612a5324
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2026-01-01 18:56:03 +0530

    Time: 14 ms (92.92%) | Memory: 9.1 MB (93.04%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -76,20 +76,23 @@ public:
     }
 
     int solveUsingSO2(string& text1, string& text2) {
-        int n = text1.length();
-        int m = text2.length();
-        vector<int> curr(m+1,0);
+        int m = text1.length();
+        int n = text2.length();
+        vector<int> curr(n+1,0);
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = 0; j <= m-1; j++) {
-
+        for (int i = m - 1; i >= 0; i--) {
+            int prevDiag = 0;
+            for (int j = n-1; j >= 0; j--) {
+                int temp = curr[j];
                 if (text1[i] == text2[j]) {
-                    curr[j] = 1 + curr[j + 1];
+                    curr[j] = 1 + prevDiag;
                 } else {
                     int option1 = curr[j];
-                    int option2 = curr[j + 1];
+                    int option2 = curr[j+1];
                     curr[j] = max(option1, option2);
                 }
+                prevDiag = temp;
+
             }
         }
 
@@ -104,7 +107,7 @@ public:
 
         // return solveUsingTab(text1, text2);
 
-        return solveUsingSO(text1, text2);
-        // return solveUsingSO2(text1, text2);
+        // return solveUsingSO(text1, text2);
+        return solveUsingSO2(text1, text2);
     }
 };
