fbf80a8cee9245f7f74e1ff9240f57ef3a4cf5bd
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2026-01-01 17:18:12 +0530

    Time: 23 ms (80.62%) | Memory: 27.5 MB (51.14%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    int solveUsingRec(string& text1, string& text2, int a, int b) {
-        if (a >= text1.length() || b >= text2.length()) {
+    int solveUsingRec(string& text1, string& text2, int index1, int index2) {
+        if (index1 >= text1.length() || index2 >= text2.length()) {
             return 0;
         }
-        if (text1[a] == text2[b]) {
-            return 1 + solveUsingRec(text1, text2, a + 1, b + 1);
+        if (text1[index1] == text2[index2]) {
+            return 1 + solveUsingRec(text1, text2, index1 + 1, index2 + 1);
         } else {
-            int option1 = solveUsingRec(text1, text2, a + 1, b);
-            int option2 = solveUsingRec(text1, text2, a, b + 1);
+            int option1 = solveUsingRec(text1, text2, index1 + 1, index2);
+            int option2 = solveUsingRec(text1, text2, index1, index2 + 1);
             return max(option1, option2);
         }
     }
-    int solveUsingMem(string& text1, string& text2, int a, int b,
+    int solveUsingMem(string& text1, string& text2, int index1, int index2,
                       vector<vector<int>>& dp) {
-        if (a >= text1.length() || b >= text2.length()) {
+        if (index1 >= text1.length() || index2 >= text2.length()) {
             return 0;
         }
 
-        if (dp[a][b] != -1) {
-            return dp[a][b];
+        if (dp[index1][index2] != -1) {
+            return dp[index1][index2];
         }
-        if (text1[a] == text2[b]) {
-            dp[a][b] = 1 + solveUsingMem(text1, text2, a + 1, b + 1, dp);
+        if (text1[index1] == text2[index2]) { 
+            dp[index1][index2] = 1 + solveUsingMem(text1, text2, index1 + 1, index2 + 1, dp);
         } else {
-            int option1 = solveUsingMem(text1, text2, a + 1, b, dp);
-            int option2 = solveUsingMem(text1, text2, a, b + 1, dp);
-            dp[a][b] = max(option1, option2);
+            int option1 = solveUsingMem(text1, text2, index1 + 1, index2, dp);
+            int option2 = solveUsingMem(text1, text2, index1, index2 + 1, dp);
+            dp[index1][index2] = max(option1, option2);
         }
 
-        return dp[a][b];
+        return dp[index1][index2];
     }
     int solveUsingTab(string& text1, string& text2) {
         int n = text1.length();
@@ -74,14 +74,37 @@ public:
 
         return curr[0];
     }
+
+    int solveUsingSO2(string& text1, string& text2) {
+        int n = text1.length();
+        int m = text2.length();
+        vector<int> curr(m+1,0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= m-1; j++) {
+
+                if (text1[i] == text2[j]) {
+                    curr[j] = 1 + curr[j + 1];
+                } else {
+                    int option1 = curr[j];
+                    int option2 = curr[j + 1];
+                    curr[j] = max(option1, option2);
+                }
+            }
+        }
+
+        return curr[0];
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
         // return solveUsingRec(text1,text2,0,0);
         // vector<vector<int>> dp(text1.length() + 1,
         //                        vector<int>(text2.length() + 1, -1));
         // return solveUsingMem(text1, text2, 0, 0, dp);
 
-        // return solveUsingTab(text1, text2);
+        return solveUsingTab(text1, text2);
 
-        return solveUsingSO(text1, text2);
+        // return solveUsingSO(text1, text2);
+        // return solveUsingSO2(text1, text2);
     }
 };
