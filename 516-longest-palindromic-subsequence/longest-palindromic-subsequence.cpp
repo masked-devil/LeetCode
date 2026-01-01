780a277aecbfa88f37722e3bfdf15da31ae5093f
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2026-01-01 20:24:17 +0530

    Time: 53 ms (71.28%) | Memory: 9.2 MB (97.89%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int solveUsingSO(string& text1, string& text2) {
-        int n = text1.length();
-        int m = text2.length();
-        vector<int> next(m+1,0);
-        vector<int> curr(m+1,0);
+    int solveUsingSO2(string& text1, string& text2) {
+        int m = text1.length();
+        int n = text2.length();
+        vector<int> curr(n+1,0);
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-
+        for (int i = m - 1; i >= 0; i--) {
+            int prevDiag = 0;
+            for (int j = n-1; j >= 0; j--) {
+                int temp = curr[j];
                 if (text1[i] == text2[j]) {
-                    curr[j] = 1 + next[j + 1];
+                    curr[j] = 1 + prevDiag;
                 } else {
-                    int option1 = next[j];
-                    int option2 = curr[j + 1];
+                    int option1 = curr[j];
+                    int option2 = curr[j+1];
                     curr[j] = max(option1, option2);
                 }
+                prevDiag = temp;
+
             }
-            next = curr;
         }
 
         return curr[0];
@@ -26,6 +27,6 @@ public:
         string r = s;
         reverse(r.begin(), r.end());
 
-        return solveUsingSO(s,r);
+        return solveUsingSO2(s,r);
     }
 };
