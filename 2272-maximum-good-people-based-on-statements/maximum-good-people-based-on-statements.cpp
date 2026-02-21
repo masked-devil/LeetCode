5e4dc41b4d6109cba96d5b0f83d98737ae82e111
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2026-02-21 20:44:41 +0530

    Time: 19 ms (94.44%) | Memory: 11.8 MB (45.56%) - LeetSync

--- 2272-maximum-good-people-based-on-statements/maximum-good-people-based-on-statements.cpp
+++ 2272-maximum-good-people-based-on-statements/maximum-good-people-based-on-statements.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     int maximumGood(vector<vector<int>>& statements) {
-        int ans = 0;
         int n = statements.size();
-        for(int mask = 1;mask<(1<<n);mask++){
-            bool isMaskValid = true;
-            for(int i=0;i<n;i++){
-                if(mask & (1<<i)){
-                    //this assumed to be truth
-                    //his statement should match with mask
-                    for(int j=0;j<n;j++){
-                        int statement=statements[i][j];
-                        if(statement!=2){
-                            bool maskVal = (mask & (1<<j));
-                            bool claimedVal = statement;
+        int ans = 0;
 
-                            if(maskVal!=claimedVal){
-                                isMaskValid=false;
+        for (int mask = 0; mask < (1<<n); mask++) {
+
+            bool isMaskValid = true;
+
+            for (int i = 0; i < n; i++) {
+
+                if (mask & (1 << i)) {
+
+                    for (int j = 0; j < n; j++) {
+                        if (statements[i][j] != 2) {
+                            int maskClaim = (mask & (1 << j)) ? 1 : 0;
+                            if (statements[i][j] != maskClaim){
+                                isMaskValid = false;
+                                break;
                             }
                         }
                     }
                 }
-
+                if(!isMaskValid){
+                    break;
+                }
             }
 
-            if(isMaskValid){
-                ans = max(ans,__builtin_popcount(mask));
+            if (isMaskValid) {
+                ans = max(ans, __builtin_popcount(mask));
             }
         }
-
-
         return ans;
     }
 };
