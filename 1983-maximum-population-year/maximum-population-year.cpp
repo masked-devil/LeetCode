f9d40bdf5999ec2ee8599756dd0c57b773d6f293
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2026-04-11 10:49:39 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (58.42%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        map<int, pair<int,int>> mp;
+        int popDiff[101] = {0};
+
         for(auto log:logs){
             int birth = log[0];
             int death = log[1];
 
-            mp[birth].first++;
-            mp[death].second++;
+            popDiff[birth - 1950]++;
+            popDiff[death - 1950]--;
         }
+        int pop = 0;
+        int maxPop = INT_MIN;
+        int year = -1;
 
-        pair<int,int> ans = {0,0};
-        vector<pair<int,int>> pre;
-        int i=0;
-
-        for(auto x:mp){
-            int alive = x.second.first - x.second.second;
-            int prevAlive = 0;
-            if(i>0) prevAlive = pre[i-1].second;
-            int totalAlive = alive + prevAlive;
-
-            if(totalAlive > ans.second){
-                ans = {x.first, totalAlive};
+        for(int i=0;i<101;i++){
+            int x = popDiff[i];
+            pop+=x;
+            if(pop>maxPop){
+                maxPop = pop;
+                year = i+1950;
             }
-
-            pre.push_back({x.first, totalAlive});
-            i++;
         }
 
-        return ans.first;
+        return year;
     }
 };
